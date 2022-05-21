e034326fab9791a0fbbbd8aa859f3559351787d4
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-21 16:12:39 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Shuffle(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                int position = random.Next(i + 1);
+                int shuffleIndex = array[position];
+                array[position] = array[i];
+                array[i] = shuffleIndex;
+            }
+        }
+    }
+}
